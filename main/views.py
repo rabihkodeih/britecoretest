5438b94ab4c12a73f8c9c1b9416c0cc451b4f61b
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-11-21 15:12:13 +0200

    Add class based views in main UI view

--- main/views.py
+++ main/views.py
@@ -1,17 +1,20 @@
 from django.urls.conf import path
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
 from britecore.settings import LOGIN_URL
 
 
-@login_required(login_url=LOGIN_URL)
-def home(request):
-    context = {}
-    return render(request, 'home.html', context)
+class MainView(View):
+    @method_decorator(login_required(login_url=LOGIN_URL))
+    def get(self, request):
+        context = {}
+        return render(request, 'home.html', context)
 
 
-urls = [path('', home, name='url_default'),
-        path('home/', home, name='url_home')]
+urls = [path('', MainView.as_view(), name='url_default'),
+        path('home/', MainView.as_view(), name='url_home')]
 
 
 # end of file
