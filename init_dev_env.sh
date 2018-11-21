2f69641a2a5a7ff199f9f26d74c5e547fe31d67b
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-11-21 14:59:06 +0200

    Update deploy script, add prod. env init script

---
+++ init_dev_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export BC_SECRET_KEY='yf*e^dqt2b4^lnf8$1kqotk&2w!-ab!nc83jl$++g-ztn8xd^+'
+export BC_DEBUG=0
+export BC_DB_NAME=britecoretest
+export BC_DB_USER=britecoretestadmin7bli
+export BC_DB_PASSWORD=fhuryg^&^%3et64%&&*derrf2390
+export BC_DB_HOST=rds-postgresql-7bli.cth7mcrqx10m.us-east-2.rds.amazonaws.com
+export BC_DB_PORT=5432
+export BC_STATIC_URL='https://zappa-static-7bli.s3.amazonaws.com/'
+export BC_STATICFILES_STORAGE='storages.backends.s3boto.S3BotoStorage'
